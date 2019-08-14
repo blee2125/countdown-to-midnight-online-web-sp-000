321bee93b4ef0d43d405157359d43ad2d34cdad0
favorable-device-5158 <blee2125@gmail.com>
2019-08-14 18:34:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,9 +4,7 @@ def countdown(seconds)
   while seconds > 0
     puts "#{seconds} SECOND(S)!"
     seconds -=1
-    if seconds == 0
-      puts "HAPPY NEW YEAR!"
 
-    end
   end
 end
+"HAPPY NEW YEAR!"
